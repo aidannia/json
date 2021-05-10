bbd0595cf7de87e1ca34d3b9fa62d1ac25b7fe62
wayfarer-ezyro <72940489+wayfarer-ezyro@users.noreply.github.com>
2021-05-10 11:21:19 +0530

    Update BuildVars.java

--- src/main/java/se/anyro/jsondumpbot/BuildVars.java
+++ src/main/java/se/anyro/jsondumpbot/BuildVars.java
@@ -1,6 +1,6 @@
 package se.anyro.jsondumpbot;
 
 public class BuildVars {
-    public static final int OWNER = // Your user id here. Send message to @userinfobot to get it.
-    public static final String TOKEN = // Your bot token here. Use @BotFather.
+    public static final int OWNER = "1453690249" // Your user id here. Send message to @userinfobot to get it.
+    public static final String TOKEN = "1854471714:AAEXujfE2HZb7K2Onz5SQkiueOdddfYx1Ro" // Your bot token here. Use @BotFather.
 }
